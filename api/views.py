cf055523d43ef02f4b38a9cb765acf536dbc89b7
Alexej Mitin <alexej.mitin@hotmail.com>
2024-01-19 20:37:41 +0100

    initial push maybe broken?

---
+++ api/views.py
@@ -0,0 +1,16 @@
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from base.models import Item
+from .serializers import RecipeSerializer
+@api_view(['GET'])
+def getData(request):
+    items = Item.objects.all()
+    serializer = RecipeSerializer(items, many=True)
+    return Response(serializer.data)
+
+@api_view(['POST'])
+def addItem(request):
+    serializer = RecipeSerializer(data=request.data)
+    if serializer.is_valid():
+        serializer.save()
+    return Response()
